d007dbbfe0ef2f0893d7d08562c2f47f54b65d86
Борисов Илья <ilya.borisov@technopark.ru>
2024-03-09 16:32:34 +0700

    Адаптив

---
+++ src/js/popup.js
@@ -0,0 +1,15 @@
+function openModal(id) {
+  document.querySelector('body').classList.add('locked');
+  document.querySelector(id).classList.add('active');
+  document.querySelector(id).addEventListener('click', ({ target }) => {
+    if (`#${target.id}` === id) {
+      console.log('хуй')
+      closeModal(id);
+    }
+  })
+}
+
+function closeModal(id) {
+  document.querySelector('body').classList.remove('locked');
+  document.querySelector(id).classList.remove('active');
+}
