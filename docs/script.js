583870fe46866cf96facc22ea26fe7987272ed11
Ilya Borisov <battersman27@gmail.com>
2024-03-14 22:35:24 +0700

    Quotes анимация

--- docs/script.js
+++ docs/script.js
@@ -224,20 +224,53 @@ function closeModal(id) {
 const quoteElement = document.querySelector('.quote');
 
 if (quoteElement) {
-  const observerQuote = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
+  const quotes = quoteElement.dataset.quotes;
+
+  if (quotes) {
+    const quoteTextElement = quoteElement.querySelector('.quote__text');
+    const quoteNameElement = quoteElement.querySelector('.quote__name');
+    const quoteImageElement = quoteElement.querySelector('.quote__image img');
+
+    const arrayOfQuotes = JSON.parse(quotes);
+    const maxIterations = arrayOfQuotes.length;
+    const observerQuote = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setQuoteData(0);
+          quoteElement.classList.add('active');
+        }
+      })
+    }, {
+      rootMargin: '0px',
+      threshold: 1,
+    });
+
+    observerQuote.observe(quoteElement);
+
+    let countQuote = 0
+    setInterval(() => {
+      quoteElement.classList.remove('active');
+      quoteElement.classList.add('disable');
+      countQuote++;
+      setTimeout(() => {
+        if (countQuote >= maxIterations) {
+          countQuote = 0;
+        }
+        quoteElement.classList.remove('disable');
         quoteElement.classList.add('active');
-      }
-    })
-  }, {
-    rootMargin: '0px',
-    threshold: 1,
-  });
+        setQuoteData(countQuote);
+      }, 800);
+    }, 8000);
 
-  observerQuote.observe(quoteElement);
+    function setQuoteData(index) {
+      quoteTextElement.innerText = arrayOfQuotes[index].text;
+      quoteNameElement.innerText = arrayOfQuotes[index].name;
+      quoteImageElement.src = arrayOfQuotes[index].image;
+    }
+  } else {
+    quoteElement.remove();
+  }
 }
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const selectElements = document.querySelectorAll('.select'),
