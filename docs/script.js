e8966770b533569419834789e7fe18dd65b68121
Ilya Borisov <battersman27@gmail.com>
2024-03-18 13:46:21 +0700

    Правки

--- docs/script.js
+++ docs/script.js
@@ -222,32 +222,29 @@ function closeModal(id) {
 
 
 const quoteElement = document.querySelector('.quote');
+const quotes = quoteElement?.dataset?.quotes;
 
-if (quoteElement) {
-  const quotes = quoteElement.dataset.quotes;
+if (quoteElement && quotes) {
+  const arrayOfQuotes = JSON.parse(quotes);
 
-  if (quotes) {
-    const quoteTextElement = quoteElement.querySelector('.quote__text');
-    const quoteNameElement = quoteElement.querySelector('.quote__name');
-    const quoteImageElement = quoteElement.querySelector('.quote__image img');
+  const observerQuote = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setQuoteData(0);
+        quoteElement.classList.add('active');
+      }
+    })
+  }, {
+    rootMargin: '0px',
+    threshold: 1,
+  });
 
-    const arrayOfQuotes = JSON.parse(quotes);
+  if (arrayOfQuotes.length > 1) {
+    console.log('quotes > 1')
     const maxIterations = arrayOfQuotes.length;
-    const observerQuote = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          setQuoteData(0);
-          quoteElement.classList.add('active');
-        }
-      })
-    }, {
-      rootMargin: '0px',
-      threshold: 1,
-    });
-
+    let countQuote = 0;
     observerQuote.observe(quoteElement);
 
-    let countQuote = 0
     setInterval(() => {
       quoteElement.classList.remove('active');
       quoteElement.classList.add('disable');
@@ -261,16 +258,32 @@ if (quoteElement) {
         setQuoteData(countQuote);
       }, 800);
     }, 8000);
+  }
 
-    function setQuoteData(index) {
-      quoteTextElement.innerText = arrayOfQuotes[index].text;
-      quoteNameElement.innerText = arrayOfQuotes[index].name;
-      quoteImageElement.src = arrayOfQuotes[index].image;
-    }
-  } else {
+  if (arrayOfQuotes.length === 1) {
+    observerQuote.observe(quoteElement);
+    setQuoteData(0);
+  }
+
+  if (arrayOfQuotes.length === 0 || !arrayOfQuotes)  {
+    quoteElement.remove();
+  }
+
+  function setQuoteData(index) {
+    const quoteTextElement = quoteElement.querySelector('.quote__text');
+    const quoteNameElement = quoteElement.querySelector('.quote__name');
+    const quoteImageElement = quoteElement.querySelector('.quote__image img');
+
+    quoteTextElement.innerText = arrayOfQuotes[index].text;
+    quoteNameElement.innerText = arrayOfQuotes[index].name;
+    quoteImageElement.src = arrayOfQuotes[index].image;
+  }
+} else {
+  if (quoteElement) {
     quoteElement.remove();
   }
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const selectElements = document.querySelectorAll('.select'),
